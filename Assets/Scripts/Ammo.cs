467941cf921266b0a58e1a3184a7d1b57c5f95d9
SheyuWu8 <jjq81146@163.com>
2023-05-07 15:51:49 +0200

    ammoReload

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 
 public class Ammo : MonoBehaviour
 {
+    public int ammoMax = 24;
     public int ammo;
     public bool isFiring;
+    public bool Empty;
     public float reloadTime = 2f;
     public Text ammoDisplay;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
@@ -25,16 +27,26 @@ public class Ammo : MonoBehaviour
 
 
         if (Input.GetMouseButtonDown(0) && !isFiring && ammo > 0)
-        {
+        { 
+            Empty = false;
             isFiring = true;
             ammo -= 3;
             isFiring = false;
+        }   
+        if(ammo == 0)
+        {
+            Empty = true;
+            Reload();
         }
-       
-
-
-       
+        
+        
       
+    }
+
+    public void Reload()
+    {
+        int reloadAmount = ammoMax - ammo;
+        ammo += reloadAmount;
     }
     
     
