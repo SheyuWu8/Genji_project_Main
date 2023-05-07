151cec4626f9d853168f5e5de96728ec63f13452
SheyuWu8 <jjq81146@163.com>
2023-05-07 16:15:10 +0200

    cd

--- Assets/Scripts/coolDown.cs
+++ Assets/Scripts/coolDown.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class coolDown : MonoBehaviour
 {
+    public Image dashCD;
+    public float coolDown1 = 0.1f;
+    bool isCooldown = false;
+    public KeyCode LeftShift;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +18,24 @@ public class coolDown : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {dashCD.fillAmount = 0;
+        dash();
+    }
+    void dash()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isCooldown == false)
+        {
+            isCooldown = true;
+            dashCD.fillAmount = 1;
+        }
+        if (isCooldown)
+        {
+            dashCD.fillAmount -= 1 / coolDown1 * Time.deltaTime;
+            if (dashCD.fillAmount <= 0)
+            {
+                dashCD.fillAmount = 0;
+                isCooldown = false;
+            }
+        }
     }
 }
